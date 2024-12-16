9642bc2309480489342d6882fa4865b16ba28501
Ivan Kosik <ivankosik91@gmail.com>
2024-12-16 17:22:14 +0300

    feat(icon): Update app icon

--- src/bsmu/macula/plugins/images/icons_rc.py
+++ src/bsmu/macula/plugins/images/icons_rc.py
@@ -6,7 +6,7 @@
 from PySide6 import QtCore
 
 qt_resource_data = b"\
-\x00\x00\x14\xc9\
+\x00\x00\x16L\
 <\
 ?xml version=\x221.\
 0\x22 encoding=\x22UTF\
@@ -61,286 +61,310 @@ pageopacity=\x220.0\
 \x22\x0a     inkscape:\
 pageshadow=\x222\x22\x0a \
     inkscape:zoo\
-m=\x221\x22\x0a     inksc\
-ape:cx=\x22201.4999\
-9\x22\x0a     inkscape\
-:cy=\x22117\x22\x0a     i\
-nkscape:document\
--units=\x22mm\x22\x0a    \
- inkscape:curren\
-t-layer=\x22layer1\x22\
-\x0a     inkscape:d\
-ocument-rotation\
-=\x220\x22\x0a     showgr\
-id=\x22false\x22\x0a     \
-fit-margin-top=\x22\
-0\x22\x0a     fit-marg\
-in-left=\x220\x22\x0a    \
- fit-margin-righ\
-t=\x220\x22\x0a     fit-m\
-argin-bottom=\x220\x22\
-\x0a     inkscape:w\
-indow-width=\x22192\
-0\x22\x0a     inkscape\
-:window-height=\x22\
-1027\x22\x0a     inksc\
-ape:window-x=\x22-8\
+m=\x220.70710678\x22\x0a \
+    inkscape:cx=\
+\x2251.618795\x22\x0a    \
+ inkscape:cy=\x2294\
+.752308\x22\x0a     in\
+kscape:document-\
+units=\x22mm\x22\x0a     \
+inkscape:current\
+-layer=\x22layer1\x22\x0a\
+     inkscape:do\
+cument-rotation=\
+\x220\x22\x0a     showgri\
+d=\x22false\x22\x0a     f\
+it-margin-top=\x220\
+\x22\x0a     fit-margi\
+n-left=\x220\x22\x0a     \
+fit-margin-right\
+=\x220\x22\x0a     fit-ma\
+rgin-bottom=\x220\x22\x0a\
+     inkscape:wi\
+ndow-width=\x221920\
 \x22\x0a     inkscape:\
-window-y=\x22-8\x22\x0a  \
-   inkscape:wind\
-ow-maximized=\x221\x22\
-\x0a     inkscape:s\
-nap-bbox=\x22true\x22\x0a\
+window-height=\x221\
+027\x22\x0a     inksca\
+pe:window-x=\x22-8\x22\
+\x0a     inkscape:w\
+indow-y=\x22-8\x22\x0a   \
+  inkscape:windo\
+w-maximized=\x221\x22\x0a\
      inkscape:sn\
-ap-object-midpoi\
-nts=\x22true\x22\x0a     \
-inkscape:snap-gl\
-obal=\x22true\x22\x0a    \
- inkscape:showpa\
-geshadow=\x220\x22\x0a   \
-  inkscape:pagec\
-heckerboard=\x220\x22\x0a\
-     inkscape:de\
-skcolor=\x22#d1d1d1\
-\x22 />\x0a  <metadata\
-\x0a     id=\x22metada\
-ta1424\x22>\x0a    <rd\
-f:RDF>\x0a      <cc\
-:Work\x0a         r\
-df:about=\x22\x22>\x0a   \
-     <dc:format>\
-image/svg+xml</d\
-c:format>\x0a      \
-  <dc:type\x0a     \
-      rdf:resour\
-ce=\x22http://purl.\
-org/dc/dcmitype/\
-StillImage\x22 />\x0a \
-     </cc:Work>\x0a\
-    </rdf:RDF>\x0a \
- </metadata>\x0a  <\
-g\x0a     inkscape:\
-label=\x22\xd0\xa1\xd0\xbb\xd0\xbe\xd0\xb9 \
-1\x22\x0a     inkscape\
-:groupmode=\x22laye\
-r\x22\x0a     id=\x22laye\
-r1\x22\x0a     transfo\
-rm=\x22translate(61\
-7.06496,-355.217\
-17)\x22>\x0a    <circl\
-e\x0a       style=\x22\
-font-variation-s\
-ettings:normal;f\
-ill:#363b34;fill\
--opacity:1;fill-\
-rule:evenodd;str\
-oke:none;stroke-\
-width:9.4257;str\
-oke-linecap:butt\
-;stroke-linejoin\
-:round;stroke-mi\
-terlimit:4;strok\
-e-dasharray:none\
-;stroke-dashoffs\
-et:0;stroke-opac\
-ity:1;stop-color\
-:#000000\x22\x0a      \
- id=\x22path70097-9\
-1-83-9-3-5-4-5-0\
--0-8-5-3-7-6-6-8\
--8-8-2-4-2-5-7-7\
--7-03-3-3-9-2-4-\
-3-6-5-2-8-0-3\x22\x0a \
-      cx=\x22-577.9\
-5142\x22\x0a       cy=\
-\x22394.33026\x22\x0a    \
-   r=\x2234.399834\x22\
- />\x0a    <path\x0a  \
-     style=\x22colo\
-r:#000000;fill:#\
-6b7468;fill-rule\
-:evenodd;stroke:\
-none;stroke-line\
-join:round\x22\x0a    \
-   d=\x22m -577.951\
-69,355.21737 c -\
-21.5455,0 -39.11\
-327,17.5677 -39.\
-11327,39.1133 0,\
-21.5455 17.56777\
-,39.1133 39.1132\
-7,39.1133 21.545\
-49,0 39.11327,-1\
-7.5678 39.11327,\
--39.1133 0,-21.5\
-456 -17.56778,-3\
-9.1133 -39.11327\
-,-39.1133 z m 0,\
-9.4258 c 16.4514\
-8,0 29.68749,13.\
-2359 29.68749,29\
-.6875 0,16.4515 \
--13.23601,29.687\
-5 -29.68749,29.6\
-875 -16.45148,0 \
--29.68749,-13.23\
-6 -29.68749,-29.\
-6875 0,-16.4516 \
-13.23601,-29.687\
-5 29.68749,-29.6\
-875 z\x22\x0a       id\
-=\x22circle152032-4\
--0-2-8-3-3-5-0-2\
--6-2-7-1-8-9-18-\
-4-6-0-2-7-3-5-3-\
-3-4-5-1\x22 />\x0a    \
-<path\x0a       id=\
-\x22path45611-9-6-9\
--2-9-9-9-4-8-3-0\
-\x22\x0a       style=\x22\
-fill:#52c25d;fil\
-l-opacity:1;stro\
+ap-bbox=\x22true\x22\x0a \
+    inkscape:sna\
+p-object-midpoin\
+ts=\x22true\x22\x0a     i\
+nkscape:snap-glo\
+bal=\x22true\x22\x0a     \
+inkscape:showpag\
+eshadow=\x220\x22\x0a    \
+ inkscape:pagech\
+eckerboard=\x220\x22\x0a \
+    inkscape:des\
+kcolor=\x22#d1d1d1\x22\
+ />\x0a  <metadata\x0a\
+     id=\x22metadat\
+a1424\x22>\x0a    <rdf\
+:RDF>\x0a      <cc:\
+Work\x0a         rd\
+f:about=\x22\x22>\x0a    \
+    <dc:format>i\
+mage/svg+xml</dc\
+:format>\x0a       \
+ <dc:type\x0a      \
+     rdf:resourc\
+e=\x22http://purl.o\
+rg/dc/dcmitype/S\
+tillImage\x22 />\x0a  \
+    </cc:Work>\x0a \
+   </rdf:RDF>\x0a  \
+</metadata>\x0a  <g\
+\x0a     inkscape:l\
+abel=\x22\xd0\xa1\xd0\xbb\xd0\xbe\xd0\xb9 1\
+\x22\x0a     inkscape:\
+groupmode=\x22layer\
+\x22\x0a     id=\x22layer\
+1\x22\x0a     transfor\
+m=\x22translate(617\
+.06496,-355.2171\
+7)\x22>\x0a    <circle\
+\x0a       style=\x22f\
+ont-variation-se\
+ttings:normal;fi\
+ll:#363b34;fill-\
+opacity:1;fill-r\
+ule:evenodd;stro\
 ke:none;stroke-w\
-idth:0.258475px;\
-stroke-linecap:b\
-utt;stroke-linej\
-oin:miter;stroke\
--opacity:1\x22\x0a    \
-   d=\x22m -595.563\
-82,393.91958 c -\
-2.23765,0.1157 -\
-4.91424,0.097 -6\
-.54448,1.9689 -1\
-.75471,2.8454 -0\
-.47359,6.3049 0.\
-56826,9.1729 0.7\
-3818,2.4645 2.72\
-553,4.4233 5.173\
-37,4.8545 7.6544\
-,1.3409 15.45026\
-,1.1839 23.18496\
-,1.1271 4.56415,\
--0.05 9.14085,-0\
-.3436 13.6452,-1\
-.1271 2.44784,-0\
-.4312 4.4352,-2.\
-39 5.17338,-4.85\
-45 1.04167,-2.86\
-81 2.32345,-6.32\
-76 0.56875,-9.17\
-29 -1.72835,-1.9\
-487 -4.54903,-1.\
-879 -6.89731,-1.\
-9689 -4.45884,0.\
-1582 -9.21765,0.\
-1787 -13.12686,2\
-.7578 -2.68651,1\
-.5403 -6.00249,1\
-.3132 -8.64119,-\
-0.2199 -3.94064,\
--2.4503 -8.67614\
-,-2.3801 -13.104\
-08,-2.5379 z\x22\x0a  \
-     sodipodi:no\
-detypes=\x22ssssscc\
-cccccsssssccsss\x22\
- />\x0a    <path\x0a  \
-     id=\x22path456\
-11-9-6-2-6-1-8-6\
--9-4-9-9\x22\x0a      \
- style=\x22fill:#ec\
-e037;fill-opacit\
-y:1;stroke:none;\
-stroke-width:0.2\
-58475px;stroke-l\
-inecap:butt;stro\
-ke-linejoin:mite\
-r;stroke-opacity\
-:1\x22\x0a       d=\x22m \
--553.92594,403.8\
-3928 c -6.1657,1\
-.2404 -15.04374,\
-2.019 -24.91304,\
-2.019 -8.89925,0\
- -16.98771,-0.63\
-38 -23.0133,-1.6\
-676 0.10448,0.29\
-38 0.20922,0.587\
-3 0.31219,0.8707\
- 0.73817,2.4645 \
-2.72561,4.4233 5\
-.17345,4.8545 7.\
-6544,1.3409 15.4\
-5023,1.1839 23.1\
-8493,1.1271 4.56\
-414,-0.05 9.1410\
-1,-0.3436 13.645\
-33,-1.1271 2.447\
-86,-0.4312 4.435\
-31,-2.39 5.17348\
-,-4.8545 0.1441,\
--0.3968 0.29234,\
--0.8059 0.43696,\
--1.2221 z\x22 />\x0a  \
-  <path\x0a       i\
-d=\x22rect51589-2-7\
--0-4-8\x22\x0a       s\
-tyle=\x22fill:#8792\
-81;fill-opacity:\
-1;stroke-width:3\
-;stroke-linecap:\
-round;stroke-lin\
-ejoin:round;pain\
-t-order:markers \
-fill stroke;stop\
--color:#000000\x22\x0a\
-       d=\x22m -577\
-.95159,355.21717\
- c -16.99881,0 -\
-31.5189,10.937 -\
-36.89129,26.1338\
- l 8.88267,3.080\
-4 c 4.05781,-11.\
-5524 15.03209,-1\
-9.7884 28.00862,\
--19.7884 12.9765\
-2,0 23.9508,8.23\
-6 28.00862,19.78\
-84 l 8.88266,-3.\
-0804 c -5.37239,\
--15.1968 -19.892\
-49,-26.1338 -36.\
-89128,-26.1338 z\
+idth:9.4257;stro\
+ke-linecap:butt;\
+stroke-linejoin:\
+round;stroke-mit\
+erlimit:4;stroke\
+-dasharray:none;\
+stroke-dashoffse\
+t:0;stroke-opaci\
+ty:1;stop-color:\
+#000000\x22\x0a       \
+id=\x22path70097-91\
+-83-9-3-5-4-5-0-\
+0-8-5-3-7-6-6-8-\
+8-8-2-4-2-5-7-7-\
+7-03-3-3-9-2-4-3\
+-6-5-2-8-0-3-0\x22\x0a\
+       cx=\x22-577.\
+95172\x22\x0a       cy\
+=\x22394.33026\x22\x0a   \
+    r=\x2234.399834\
 \x22 />\x0a    <path\x0a \
-      id=\x22path70\
-41\x22\x0a       style\
-=\x22fill:#f17e37;s\
-troke-width:2.78\
-212;stroke-linec\
+      style=\x22col\
+or:#000000;fill:\
+#6b7468;fill-rul\
+e:evenodd;stroke\
+:none;stroke-lin\
+ejoin:round\x22\x0a   \
+    d=\x22m -577.95\
+169,355.21737 c \
+-21.5455,0 -39.1\
+1327,17.5677 -39\
+.11327,39.1133 0\
+,21.5455 17.5677\
+7,39.1133 39.113\
+27,39.1133 21.54\
+549,0 39.11327,-\
+17.5678 39.11327\
+,-39.1133 0,-21.\
+5456 -17.56778,-\
+39.1133 -39.1132\
+7,-39.1133 z m 0\
+,9.4258 c 16.451\
+48,0 29.68749,13\
+.2359 29.68749,2\
+9.6875 0,16.4515\
+ -13.23601,29.68\
+75 -29.68749,29.\
+6875 -16.45148,0\
+ -29.68749,-13.2\
+36 -29.68749,-29\
+.6875 0,-16.4516\
+ 13.23601,-29.68\
+75 29.68749,-29.\
+6875 z\x22\x0a       i\
+d=\x22circle152032-\
+4-0-2-8-3-3-5-0-\
+2-6-2-7-1-8-9-18\
+-4-6-0-2-7-3-5-3\
+-3-4-5-1-7\x22 />\x0a \
+   <path\x0a       \
+id=\x22rect51589-2-\
+7-0-4-8-6\x22\x0a     \
+  style=\x22fill:#8\
+79281;fill-opaci\
+ty:1;stroke-widt\
+h:3;stroke-linec\
 ap:round;stroke-\
 linejoin:round;p\
 aint-order:marke\
 rs fill stroke;s\
 top-color:#00000\
 0\x22\x0a       d=\x22m -\
-577.95159,376.15\
-593 a 15.52836,1\
-5.52836 0 0 0 -1\
-5.51429,15.18615\
- c 3.79331,0.129\
-69 7.68219,0.402\
-83 11.00604,2.46\
-962 2.6387,1.533\
-1 5.95483,1.7599\
-2 8.64134,0.2196\
-2 3.40551,-2.246\
-79 7.45547,-2.55\
-161 11.39052,-2.\
-69751 a 15.52836\
-,15.52836 0 0 0 \
--15.52361,-15.17\
-788 z\x22 />\x0a  </g>\
-\x0a</svg>\x0a\
+577.95159,355.21\
+717 c -16.99881,\
+0 -31.5189,10.93\
+7 -36.89129,26.1\
+338 l 8.88267,3.\
+0804 c 4.05781,-\
+11.5524 15.03209\
+,-19.7884 28.008\
+62,-19.7884 12.9\
+7652,0 23.9508,8\
+.236 28.00862,19\
+.7884 l 8.88266,\
+-3.0804 c -5.372\
+39,-15.1968 -19.\
+89249,-26.1338 -\
+36.89128,-26.133\
+8 z\x22 />\x0a    <pat\
+h\x0a       id=\x22pat\
+h1026\x22\x0a       st\
+yle=\x22fill:#52c25\
+d;fill-opacity:1\
+;stroke:none;str\
+oke-width:0.2658\
+44px;stroke-line\
+cap:butt;stroke-\
+linejoin:miter;s\
+troke-opacity:1\x22\
+\x0a       d=\x22m -59\
+6.52412,393.8953\
+1 c -3.71022,-0.\
+0316 -8.00142,-0\
+.55989 -7.98692,\
+2.00803 0.0258,4\
+.5739 1.29178,9.\
+12441 4.00399,12\
+.74855 0.69334,0\
+.92647 2.05071,1\
+.10078 3.13546,1\
+.28148 8.07585,1\
+.34526 16.30129,\
+1.1869 24.46199,\
+1.12995 4.81555,\
+-0.0501 9.64468,\
+-0.3426 14.39682\
+,-1.12995 1.0849\
+,-0.17976 2.4421\
+2,-0.355 3.13546\
+,-1.28148 2.7123\
+7,-3.62443 3.935\
+57,-7.98956 4.00\
+399,-12.74855 0.\
+0334,-2.32321 -6\
+.29933,-2.06144 \
+-8.35824,-2.0080\
+3 -4.70551,0.122\
+06 -9.72539,-0.0\
+409 -13.84991,2.\
+54489 -2.83448,1\
+.54431 -6.33282,\
+1.5371 -9.11685,\
+0 -4.15768,-2.45\
+668 -10.72497,-2\
+.50244 -13.82582\
+,-2.54489 z\x22\x0a   \
+    sodipodi:nod\
+etypes=\x22cssscsss\
+scccc\x22 />\x0a    <p\
+ath\x0a       id=\x22p\
+ath1105\x22\x0a       \
+style=\x22font-vari\
+ation-settings:n\
+ormal;opacity:1;\
+vector-effect:no\
+ne;fill:#ece037;\
+fill-opacity:1;s\
+troke:none;strok\
+e-width:0.353255\
+px;stroke-lineca\
+p:butt;stroke-li\
+nejoin:miter;str\
+oke-miterlimit:4\
+;stroke-dasharra\
+y:none;stroke-da\
+shoffset:0;strok\
+e-opacity:1;-ink\
+scape-stroke:non\
+e;stop-color:#00\
+0000;stop-opacit\
+y:1\x22\x0a       d=\x22m\
+ -552.76719,403.\
+83852 a 65.29394\
+5,18.895956 0 0 \
+1 -25.18399,1.46\
+399 65.293945,18\
+.895956 0 0 1 -2\
+5.15712,-1.46037\
+ c 0.63809,1.715\
+61 1.50067,3.338\
+71 2.60139,4.809\
+52 0.69334,0.926\
+47 2.05046,1.100\
+88 3.13521,1.281\
+58 8.07584,1.345\
+26 16.30138,1.18\
+711 24.46207,1.1\
+3016 4.81555,-0.\
+0501 9.64492,-0.\
+34281 14.39706,-\
+1.13016 1.0849,-\
+0.17976 2.44187,\
+-0.3551 3.13521,\
+-1.28158 1.11328\
+,-1.48763 1.9755\
+2,-3.09993 2.610\
+17,-4.81314 z\x22 /\
+>\x0a    <path\x0a    \
+   id=\x22path1202\x22\
+\x0a       style=\x22f\
+ont-variation-se\
+ttings:normal;op\
+acity:1;vector-e\
+ffect:none;fill:\
+#f17e37;fill-opa\
+city:1;stroke:no\
+ne;stroke-width:\
+2.78212;stroke-l\
+inecap:round;str\
+oke-linejoin:rou\
+nd;stroke-miterl\
+imit:4;stroke-da\
+sharray:none;str\
+oke-dashoffset:0\
+;stroke-opacity:\
+1;-inkscape-stro\
+ke:none;paint-or\
+der:markers fill\
+ stroke;stop-col\
+or:#000000;stop-\
+opacity:1\x22\x0a     \
+  d=\x22m -577.9511\
+8,376.2066 a 15.\
+742708,15.742708\
+ 0 0 0 -15.71687\
+,15.11174 c 3.35\
+323,0.1444 7.842\
+7,0.62803 10.969\
+87,2.47581 2.784\
+03,1.5371 6.2822\
+8,1.54431 9.1167\
+6,0 3.40308,-2.1\
+335 7.41531,-2.3\
+9526 11.35796,-2\
+.49028 a 15.7427\
+08,15.742708 0 0\
+ 0 -15.72772,-15\
+.09727 z\x22 />\x0a  <\
+/g>\x0a</svg>\x0a\
 "
 
 qt_resource_name = b"\
@@ -360,7 +384,7 @@ qt_resource_struct = b"\
 \x00\x00\x00\x00\x00\x02\x00\x00\x00\x01\x00\x00\x00\x02\
 \x00\x00\x00\x00\x00\x00\x00\x00\
 \x00\x00\x00\x10\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\
-\x00\x00\x01\x93@rc\xcf\
+\x00\x00\x01\x93\xbb\xecbm\
 "
 
 def qInitResources():
