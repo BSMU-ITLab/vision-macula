e276c95afbb4c82fa1ebd8e20610bb8fc8bc0918
Fando Evgeniy <fando.evgeniy@gmail.com>
2026-09-11 11:09:41 +0300

    fix merge

--- src/bsmu/macula/plugins/analyser/analyzed_table.py
+++ src/bsmu/macula/plugins/analyser/analyzed_table.py
@@ -43,7 +43,6 @@ class ObjectsTableModel(QAbstractTableModel):
     HEADERS = [
         "Параметр",
         "Значение",
-        "Единица",
     ]
     
     # Параметры которые должны быть редактируемыми
