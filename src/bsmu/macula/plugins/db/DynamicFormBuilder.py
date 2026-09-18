c6b669a95035ff4d1cb7c805d147d90cec3a3203
Fando Evgeniy <fando.evgeniy@gmail.com>
2026-09-18 11:41:44 +0300

    - 1 pandas

--- src/bsmu/macula/plugins/db/DynamicFormBuilder.py
+++ src/bsmu/macula/plugins/db/DynamicFormBuilder.py
@@ -1,7 +1,7 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QStandardItemModel, QStandardItem, QFont
 from PySide6.QtSql import QSqlQuery
-import pandas as pd
+# import pandas as pd
 from PySide6.QtWidgets import QLineEdit, QComboBox, QPushButton, QGroupBox, QFormLayout, QWidget, QVBoxLayout, \
     QTableView, QLabel
 
